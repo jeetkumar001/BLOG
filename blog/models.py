82db976b72723c881b9fb84315fdc7344b124bb8
Jeet <jeetkumar6992@gmail.com>
2021-04-03 00:40:51 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User , on_delete = models.CASCADE)
+    content = models.TextField()
+    title = models.CharField(max_length = 100)
+    date_posted = models.DateTimeField(default = timezone.now)
+       
+    def __str__(self):
+            return self.title
+
+
